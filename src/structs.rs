c35fa68667dc54d946a79a56fba66cac865a7406
Alessio Amiri <alessioamiri182@gmail.com>
2024-11-19 09:54:36 +0100

    look_up key v1

--- src/structs.rs
+++ src/structs.rs
@@ -1,62 +1,107 @@
+use digest::{Digest, Output};
 use std::io;
 use std::marker::PhantomData;
-use std::net::{IpAddr, Ipv4Addr};
-use sha2::{Sha256, Digest};
+use std::ops::Deref;
+use std::sync::Arc;
 
-//todo redo
 pub trait ChordSend<T>{
-
-
     async fn send(&self, message: T) -> io::Result<()>;
-
+    async fn send_string(&self, message: &str) -> io::Result<()>;
     async fn receive(&self)->Option<T>;
-
-    fn get_sha_dimension()->usize;
-
-    fn use_sha(){
-
-    }
+    async fn receive_string(&self)->Option<String>;
+    fn to_be_bytes(&self)->[u8];
 }
 
-pub struct ChordNode<A, T> where A: ChordSend<T>
+pub struct ChordNode<A, T, D> where A: ChordSend<T>+std::convert::AsRef<[u8]>, D: Digest+Clone
 {
-    pub id: [u8; 32],
-    pub address: A,
-    pub predecessor: A,
-    pub finger_table: Vec<A>,
+    pub id: Output<D>,
+    pub address: Arc<A>, //todo: mutex?
+    pub predecessor: Option<Arc<Self>>, //todo: mutex?
+    pub finger_table: Vec<Arc<Self>>,
+    sha: D,
     phantom: PhantomData<T>,
 }
 
-
-impl<A, T> ChordNode<A, T> where A: ChordSend<T>{
-
-    //todo
-    pub fn new(ip_addr: IpAddr)->Self<A, T>{
-        Self
+impl<A, T, D> ChordNode<A, T, D> where A: ChordSend<T>+std::convert::AsRef<[u8]>,  D: Digest+Clone{
+    pub fn new(self_reference: A, sha: D) ->Self{
+        let reference= Arc::new(self_reference);
+        let id= sha.clone().chain_update(reference.deref()).finalize();
+        ChordNode{
+            id,
+            address: reference.clone(),
+            predecessor: None,
+            finger_table: vec![],
+            sha,
+            phantom: Default::default(),
+        }
     }
-
-    pub fn sha_length(mut self)->Self<A, T>{
-        Self
-    }
-
 
     pub async fn send_to_successor(&self, message: T){
-        println!("{:?}", self.finger_table[0].send(message).await)
+        if self.finger_table.is_empty() {
+            log::error!("Finger table is empty, no successor available");
+            return;
+        }
+        println!("{:?}", self.finger_table[0].address.send(message).await)
     }
 
-    pub async fn send_to_predecessor(&self, message: T){
-        println!("{:?}", self.predecessor.send(message).await)
+    pub async fn send_to_predecessor(&mut self, message: T){
+        if let Some(predecessor) = self.predecessor.as_ref() {
+            println!("{:?}", predecessor.address.send(message).await)
+        }
+        else{
+            log::error!("No predecessor available");
+            return;
+        }
     }
 
-    pub fn look_up_key(&self, object_key: T){
+    pub async fn insert_value(&self){
 
     }
 
-    pub fn stabilize(){
+    pub async fn look_up_key(&self, key: Box<Output<D>>)->Option<Arc<A>>{
+        loop {
+            //todo: remove this loop o mmake it iterative
 
+            if self.id == *key {
+                return Some(self.address.clone());
+            }
+
+            let mut next_hop = None;
+
+            for finger in self.finger_table.iter().rev() { //todo binary search
+                if finger.id < *key  {
+                    next_hop = Some(finger.clone());
+                    break;
+                }
+            }
+
+            return if let Some(next) = next_hop {
+                //routing
+                if next.address.clone().send_string("Searching for:").await.is_ok()
+                {
+                    log::debug!("Delegation key searching message sent successfully");
+                } else {
+                    log::error!("Error while trying to send delegation key searching message");
+                }
+                next.look_up_key(key).await
+            } else {
+                // Could not route further; return failure.
+
+                None
+            }
+        }
     }
 
-    pub fn join(&mut self, node_to_add: Self){
+    pub async fn stabilize(){
+        //send message to next node and if it doesn't respond in 10 second use
+        // finger table to find the new next and update finger table
+        //once the finger table is update send the death of the node
+    }
+
+    pub async fn join(&mut self, mut node_to_add: Self){
+        let id= self.sha.clone().chain_update(&node_to_add.address.deref()).finalize();
+        node_to_add.id = id;
+        node_to_add.sha=self.sha.clone();
 
     }
 }
