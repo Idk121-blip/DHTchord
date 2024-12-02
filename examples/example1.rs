b0d5520158716053f07df7d6c6d9c3f2d813296d
andreastedile <andrea.stedile@studenti.unitn.it>
2024-12-02 11:14:27 +0100

    used thread scope for proper thread management

--- examples/example1.rs
+++ examples/example1.rs
@@ -1,26 +1,44 @@
+use std::io::Error;
 use std::net::IpAddr;
 use std::thread::sleep;
 use std::time::Duration;
 use std::{io, thread};
 use DHTchord::node_state::NodeState;
 
-pub fn main() -> Result<(), io::Error> {
-    let z = NodeState::new(IpAddr::V4("127.0.0.1".parse().unwrap()), "8911".parse().unwrap())?;
-    thread::spawn(move || {
-        z.run();
+pub fn main() {
+    thread::scope(|scope| {
+        scope.spawn(|| {
+            //
+            match NodeState::new(IpAddr::V4("127.0.0.1".parse().unwrap()), "8911".parse().unwrap()) {
+                Ok(server1) => {
+                    server1.run();
+                }
+                Err(error) => {
+                    eprintln!("{:?}", error)
+                }
+            }
+        });
+        scope.spawn(|| {
+            //
+            match NodeState::new(IpAddr::V4("127.0.0.1".parse().unwrap()), "8910".parse().unwrap()) {
+                Ok(server2) => {
+                    server2.run();
+                }
+                Err(error) => {
+                    eprintln!("{:?}", error)
+                }
+            }
+        });
+        scope.spawn(|| {
+            //
+            match NodeState::new(IpAddr::V4("127.0.0.1".parse().unwrap()), "9000".parse().unwrap()) {
+                Ok(server3) => {
+                    server3.run();
+                }
+                Err(error) => {
+                    eprintln!("{:?}", error)
+                }
+            }
+        });
     });
-
-    sleep(Duration::from_secs(2));
-    let y = NodeState::new(IpAddr::V4("127.0.0.1".parse().unwrap()), "8910".parse().unwrap())?;
-    thread::spawn(move || {
-        y.run();
-    });
-
-    sleep(Duration::from_secs(2));
-    let x = NodeState::new(IpAddr::V4("127.0.0.1".parse().unwrap()), "9000".parse().unwrap())?;
-
-    x.run();
-    //sleep(Duration::new(1000, 10));
-
-    Ok(())
 }
